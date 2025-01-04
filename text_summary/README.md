4aa2aa76cac9cb36be9fac9ab94402c0397e76a2
Jharna Aggarwal <jharnadce@gmail.com>
2025-01-04 14:08:47 -0800

    first project - website summary

---
+++ text_summary/README.md
@@ -0,0 +1,12 @@
+# Text Summarization desktop app
+
+Different kind of summarization options
+1. User can enter name of website and get summary of the webpage contents
+2. Subject line for email
+3. Title for an article
+
+## Packages
+1. OpenAI API
+2. Pyside
+
+Uses software best practices and OOP principles
---
+++ text_summary/text_summary.py
@@ -0,0 +1,98 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+from bs4 import BeautifulSoup
+import requests
+import os
+
+class APIClient:
+    """Setup API Key and modules for to the chat completion calls"""
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv("OPENAI_API_KEY")
+        self.validate_api_key()
+
+        # Create instance of OpenAI() class
+        self.client = OpenAI()
+
+    def validate_api_key(self):
+        if not self.api_key:
+            raise ValueError("API key not found")
+        elif not self.api_key.startswith("sk-proj"):
+            raise ValueError("API key found, but it does not start with sk-proj")
+        
+        print("API key validated successfully")
+
+    def chat_completion(self, model, messages):
+        response = self.client.chat.completions.create(
+            model = model,
+            messages = messages
+        )
+        return response.choices[0].message.content
+
+class WebsiteScraper:
+    """Fetch and parse website content"""
+    def __init__(self, url):
+        self.url = url
+        self.title, self.text = self._scrape_website()
+
+    def _scrape_website(self):
+        response = requests.get(self.url)
+        if response.status_code != 200:
+            raise ConnectionError(f"Failed to fetch the website. \
+                                  Status code: {response.status_code}")
+        
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.title.string if soup.title else "No title found"
+        
+        # Remove irrelavant content from website body
+        for irrelevant in soup.body(['script', 'style', 'img', 'input']):
+            irrelevant.decompose()
+        
+        text = soup.body.get_text(separator="\n", strip=True) if soup.body \
+                else "No content found"
+        return title, text
+
+
+# 3. call chat completion, mention model, link with website text
+class TextSummarizer:
+    """Combine website scraper to create prompt and call api"""
+    def __init__(self, url):
+        self.scraper = WebsiteScraper(url)
+        self.api_client = APIClient()
+        
+    def create_prompt(self):
+        # 1. define system and user prompt
+        # 2. create prompt using sytem and user prompt
+        system_prompt = (
+            "You are an expert AI assistant. "
+            "Given text from a website, provide the user with a short summary of the website. "
+            "Ignore unnecessary or navigational text. Respond in markdown."
+        )
+        user_prompt = (
+            f"Here is some content from this website titled {self.scraper.title}"
+            "The content is as follows.\n"
+            f"{self.scraper.text}"
+            "Provide a summary of the key points in markdown. "
+            "If you see any news or announcements, include a summary about that too."
+            
+        )
+
+        return [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ]
+
+    def summarize(self):
+        
+        prompt = self.create_prompt()
+        response = self.api_client.chat_completion(model="gpt-4o-mini", messages=prompt)
+        return response
+
+if __name__ == "__main__":
+    try:
+        summarizer = TextSummarizer(url="https://edwarddonner.com")
+        summary = summarizer.summarize()
+        print(summary)
+    except Exception as e:
+        print(f"An error occurred. {e}")
+
